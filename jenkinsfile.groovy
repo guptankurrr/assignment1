c363a9315d963ef038e37df8b024d0663046a1fe
guptankurrr <guptankurrr@gmail.com>
2024-11-12 14:16:10 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,8 +6,25 @@ pipeline {
     }
 
     stages {
+
+             stage('SonarQube Analysis') {
+            steps {
+                script {
+                 
+                    sh """
+                        mvn clean verify sonar:sonar -Dsonar.projectKey=my-project-key \
+                        -Dsonar.host.url=http://localhost:9000 \
+                        -Dsonar.login=${SONARQUBE_TOKEN}
+                    """
+                     
+                }
+            }
+        }
         
     }
-
-   
+ 
+    qualityGate {
+        
+        failBuild: true
+       }
 }
