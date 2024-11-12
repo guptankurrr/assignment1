ee24845f21cdbf74fc92fa41fccb937868adc1c9
guptankurrr <guptankurrr@gmail.com>
2024-11-12 20:34:06 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,11 @@
 pipeline {
     agent any  
     
+    environment {
+        SONARQUBE_SERVER = 'SonarQube'  // This should match the name of your SonarQube server configuration in Jenkins
+        SONARQUBE_TOKEN = credentials('sonar-token')  // Jenkins credential for SonarQube token
+    }
+
     triggers {
          
         pollSCM('H/5 * * * *')   
@@ -15,7 +20,37 @@ pipeline {
         }
         
    
- 
+        stage('SonarQube Analysis') {
+            steps {
+                script {
+                   
+                    echo 'Running SonarQube analysis...'
+                    withSonarQubeEnv(SONARQUBE_SERVER) {
+                        sh """
+                            sonar-scanner \
+                                -Dsonar.projectKey=my-project-key \
+                                -Dsonar.sources=. \
+                                -Dsonar.host.url=http://sonarqube-server-url \
+                                -Dsonar.login=${SONARQUBE_TOKEN}  // Use SonarQube token for authentication
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Quality Gate') {
+            steps {
+                script {
+                    
+                    echo 'Checking SonarQube quality gate status...'
+                    def qualityGate = waitForQualityGate()
+                    if (qualityGate.status != 'OK') {
+                        error "Quality gate failed: ${qualityGate.status}"   
+                    }
+                }
+            }
+        }
+
     }
 
     post {
